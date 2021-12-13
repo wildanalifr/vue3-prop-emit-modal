08da37321d24f7f6a9e62027b1f254b1a0a094c9
BB\Wildan <wildanalifrio@gmail.com>
2021-12-13 15:20:20 +0700

    first-update

---
+++ src/components/Alert.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{pesan}}</div>
+</template>
+
+<script>
+export default {
+  props: ["halo"],
+  watch: {
+    halo: function (newVal) {
+      this.pesan = newVal;
+    },
+  },
+  setup(props) {
+    let pesan = props.halo;
+    return {
+      pesan,
+    };
+  },
+};
+</script>
